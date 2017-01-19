15cfa21d6a47e19b8b3cbcac3c43ef14dd68666a
vibrito <vibrito@gmail.com>
2017-01-19 01:37:26 -0200

    first commit

---
+++ GameplayScene.swift
@@ -0,0 +1,14 @@
+import SpriteKit
+
+class GameplayScene: SKScene
+{
+    override func didMove(to view: SKView)
+    {
+        print("this scene was loaded")
+    }
+    
+    override func update(_ currentTime: TimeInterval)
+    {
+
+    }
+}
--- Jack The Giant/GameViewController.swift
+++ Jack The Giant/GameViewController.swift
@@ -10,14 +10,16 @@ import UIKit
 import SpriteKit
 import GameplayKit
 
-class GameViewController: UIViewController {
-
-    override func viewDidLoad() {
+class GameViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
+        if let view = self.view as! SKView?
+        {
+            if let scene = GameplayScene(fileNamed: "GameplayScene")
+            {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
                 
@@ -32,24 +34,31 @@ class GameViewController: UIViewController {
         }
     }
 
-    override var shouldAutorotate: Bool {
+    override var shouldAutorotate: Bool
+    {
         return true
     }
 
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask
+    {
+        if UIDevice.current.userInterfaceIdiom == .phone
+        {
             return .allButUpsideDown
-        } else {
+        }
+        else
+        {
             return .all
         }
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
 
-    override var prefersStatusBarHidden: Bool {
+    override var prefersStatusBarHidden: Bool
+    {
         return true
     }
 }
